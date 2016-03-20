311d7d3714ec76377b01a818ed139859841f53f2
otti <otti_ow@gmx.at>
2016-03-20 14:25:29 +0100

    reading data completed

--- R-solution.R
+++ R-solution.R
@@ -4,9 +4,15 @@ library('ggplot2')
 data_name = './data/household_power_consumption.txt'
 
 check_setup <- function(){
+    # check if data directory exists, if not create
     if (!dir.exists('./data')){
         dir.create('./data')
     }
+    # check if img directory for output exits, if not create
+    if(!dir.exists('./img')){
+        dir.create('./img')
+    }
+    # check if data is there, if not, download and unzip
     if (!file.exists(data_name)){
         print("Downloading the data")
         dowload.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip',
@@ -19,7 +25,7 @@ check_setup <- function(){
 
 readData <-function(fname){
     # read and clean the data. Not available data points are filled with a ?
-    data <- read.csv(fname, na.strings='?')
+    data <- read.table(fname, header=TRUE, na.strings='?', sep=';')
 }
 
 
@@ -27,7 +33,7 @@ readData <-function(fname){
 
 gap <- function(data){
     # open the png device
-    png()
+    png(file='./img/plot1.png')
     # close the device
     dev.off()
 }
